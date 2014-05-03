8b631409aa8e7a4ef4d45d873ef4e3e708544b83
sheva29 <design@sanchezmauricio.com>
2014-05-03 16:03:52 -0400

    Fixed the circle position

--- new_prototype3/js/main2.js
+++ new_prototype3/js/main2.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function (e) {
 	var canvas = Raphael("container", 500, 500);
 	canvas.canvas.id = "second-exercise";
 	var $canvasHandler = $("#second-exercise");
+	//This is our circle
+	var newCircle;
 	// var line = canvas.path(["M", 0, 25, "L", 500, 25]);
 	// var circle = canvas.circle(250, 250, 50).attr({
 	// 	fill: "#f0f0f0",
@@ -12,6 +14,8 @@ $(document).ready(function () {
 	// drawVerticalLines(); 
 	// console.log(drawVerticalLines);
 	drawLines();
+	drawCircle();
+	console.log(newCircle);
 
 	function drawLines() {
 		//This draws horizontal lines
@@ -26,23 +30,29 @@ $(document).ready(function () {
 		}
 	}
 	$canvasHandler.mousemove(function (e) {
-		console.log(e.clientX);
+		// console.log(e.clientX);
 		// var circle = canvas.circle(e.offsetX, e.offsetY, 25).attr({
 		// 	fill: "red",
 		// 	stroke: "2px"
 		// });
-		drawCircle(e.offsetX, e.offsetY);
+		updateCirclePos(e.offsetX, e.offsetY);
 	})
 
-	function drawCircle(posX, posY) {
-		var circleSet = canvas.set();
-		// if( circleSet.)
-		console.log(circleSet.length);
+	function drawCircle() {
+		newCircle = canvas.circle(12.5, 12.5, 10).attr({
+			fill: "blue",
+			stroke: "2px",
+			opacity: .8
+		});
+	}
+
+	function updateCirclePos(posX, posY) {
 		for (var i = 0; i < 500; i += 25) {
 			for (var j = 0; j < 500; j += 25) {
 				if (posX > i && posX < i + 25) {
 					if (posY > j && posY < j + 25) {
-						circleSet.push(canvas.circle(i + 12.5, j + 12.5, 12.5));
+						newCircle.node.cx.baseVal.value = i + 12.5;
+						newCircle.node.cy.baseVal.value = j + 12.5;
 					}
 				}
 			}
