656e1cd1881d3a21ec65837053442253c767ce72
sheva29 <design@sanchezmauricio.com>
2014-05-04 00:10:30 -0400

    Started box prototype

---
+++ new_prototype4/js/main3.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	var canvas = Raphael("container", 500, 500);
+	var rectPosX = 0,
+		rectPosY = 0,
+		rectWidth = 50,
+		rectHeight = 100,
+		rect2PosX = rectPosX + rectWidth,
+		rect2PosY = rectPosY;
+	rect2Width = 30,
+	rect2Height = 100;
+	var rectangle = canvas.rect(rectPosX, rectPosY, rectWidth, rectHeight).attr({
+		fill: "red"
+	});
+	var rectangleHandler = canvas.rect(30, 80, 20, 20).attr({
+		fill: "hsb(.8, .5, .5)"
+	});
+	// var rectangle2 = canvas.rect(rect2PosX, rect2PosY, rect2Width, rect2Height).attr({
+	// 	fill: "blue"
+	// });
+	rstart = function () {
+		this.ox = this.attr("x");
+		this.oy = this.attr("y");
+		this.box.ow = this.box.attr("width");
+		this.box.oh = this.box.attr("height");
+	},
+	rmove = function (dx, dy) {
+		this.attr({
+			x: this.ox + dx
+			// y: this.oy + dy
+		});
+		this.box.attr({
+			width: this.box.ow + dx
+			// height: this.box.oh + dy
+		});
+	};
+	rectangleHandler.drag(rmove, rstart);
+	rectangleHandler.box = rectangle;
+	// rectangle.transform("t" + rectPosX ,250");
+});
